d3a71d6a049b56d4a22761a41f688e726a7eef7b
cachala <104789086+cachala@users.noreply.github.com>
2023-07-31 20:24:20 +0800

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -6,7 +6,7 @@ import dataloader
 import model
 import label_utils
 from torchvision import transforms
-from engine import train_one_epoch
+from engine import train_one_epoch, evaluate
 
 if __name__ == '__main__':
     # train on the GPU or on the CPU, if a GPU is not available
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     # dataset has four classes 
     num_classes = 4
 
-    path_train = "./drinks/labels_train.csv"
+    path_train = "./drinks/drinks/labels_train.csv"
     train_dict = label_utils.build_label_dictionary(path_train)
     # use our dataset 
     dataset = dataloader.ImageDataset(train_dict, transforms.ToTensor())
@@ -25,6 +25,18 @@ if __name__ == '__main__':
                                               shuffle=True, 
                                               num_workers=2,
                                               collate_fn=utils.collate_fn)
+    path_test = "./drinks/drinks/labels_test.csv"
+    test_dict = label_utils.build_label_dictionary(path_test)
+    # use our dataset 
+    dataset_test = dataloader.ImageDataset(test_dict, transforms.ToTensor())
+    # define testing data loaders
+    data_loader_test = torch.utils.data.DataLoader(dataset_test, 
+                                                   batch_size=1, 
+                                                   shuffle=False, 
+                                                   num_workers=2,
+                                                  collate_fn=utils.collate_fn)
+   # print("TRAIN DICT", train_dict)
+   # print("TEST DICT",test_dict)
 
     # get the model
     model = model.get_model_instance_segmentation(num_classes)
@@ -38,10 +50,15 @@ if __name__ == '__main__':
     lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
 
     # modify for the number of epochs
-    num_epochs = 1
+    num_epochs = 10
 
     for epoch in range(num_epochs):
       # train for one epoch, printing every 10 iterations
       train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
       # update the learning rate
       lr_scheduler.step()
+
+      # evaluate on the test dataset
+      evaluate(model, data_loader_test, device=device)
+
+      torch.save(model, "saved_model.pth")
