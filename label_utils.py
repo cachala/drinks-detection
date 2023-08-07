7fc41a513ba0351fab189b726e38027b42019494
cachala <104789086+cachala@users.noreply.github.com>
2023-08-08 06:15:37 +0800

    Update label_utils.py

--- label_utils.py
+++ label_utils.py
@@ -51,9 +51,9 @@ def class2index(class_="background"):
 
 
 def load_csv(path):
-  #  url = 'https://github.com/ricae/Object-Detection/releases/download/v1.1/drinks.zip'
-  #  response = requests.get(url)
-  #  open('drinks.zip', 'wb').write(response.content)
+    url = 'https://github.com/ricae/Object-Detection/releases/download/v1.1/drinks.zip'
+    response = requests.get(url)
+    open('drinks.zip', 'wb').write(response.content)
     with zipfile.ZipFile('drinks.zip', 'r') as response:
         response.extractall('./drinks')
     data = []
